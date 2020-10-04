e77dd0fe2c5de11ee447c11e2923622405290215
Ahmad <ihsanashi@gmail.com>
2020-10-04 09:41:56 +0800

    added persistent state on page refresh with localstorage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Head from 'next/head';
 import Greeting from '../components/greeting';
 import styles from '../styles/Home.module.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddLocation from '../components/add-location';
 import LocationTile from '../components/location-tile';
 
 export default function Home() {
   const [locationList, setLocationList] = useState([]);
+
+  useEffect(() => {
+    const data = localStorage.getItem('loc-data');
+    if (data) {
+      setLocationList(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('loc-data', JSON.stringify(locationList));
+  });
   return (
     <div className={styles.container}>
       <Head>
