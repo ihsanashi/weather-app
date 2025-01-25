61058693a4be8a5225484a18462ab6973242412f
Ahmad Ihsan <ihsanashi@gmail.com>
2025-01-25 15:49:42 +0800

    build: update packages, use pnpm, update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: require('next-pwa/cache'),
+});
 
 module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    runtimeCaching,
-  },
+  reactStrictMode: true,
 });
